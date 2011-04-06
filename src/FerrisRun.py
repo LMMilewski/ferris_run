7c1a6c8f8bb52a99cb235f31a521728f29eb7a87
Marek Ozimina <marek@redguardian.pl>
2011-04-06 22:58:37 +0000

    Rotation problem

--- src/FerrisRun.py
+++ src/FerrisRun.py
@@ -1,5 +1,6 @@
 import os, sys;
 import pygame;
+import time;
 import cop;
 
 if not pygame.font: print "No font support";
@@ -16,22 +17,25 @@ class FerrisRunMain:
     def LoadCops(self, x):
         self.cops = [];
         for i in range(1, x + 1):
-            self.cops.append(cop.Cop(i * 100, i * 100, i * 200, i * 100));
+            self.cops.append(cop.Cop(i * 200, i * 100 + 50, i * 100, i * 100));
     
     def run_game(self):
-        self.LoadCops(3);
+        self.LoadCops(1);
+        t = time.time();
         while 1:
+            dt = time.time() - t;
+            t = time.time();
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     print "Exiting game"; 
                     sys.exit();
             sprites_to_update = pygame.sprite.Group();
             for cop in self.cops:
-                cop.update(1);
+                cop.update(dt);
                 sprites_to_update.add(cop.GetSprite());
             
             sprites_to_update.update();
-            self.screen.fill((146, 218, 255));
+            self.screen.fill((17, 157, 60));
             sprites_to_update.draw(self.screen);
             pygame.display.flip();
 
--- src/cop.py
+++ src/cop.py
@@ -1,33 +1,32 @@
 import pygame
 import math
-import random
 
 class CopSprite(pygame.sprite.Sprite):
-    def __init__(self, x, y):
+    def __init__(self, x, y, alpha):
         self.r = 25;
+        if (alpha > 180):
+            self.alpha = -alpha + 180;
+        else:
+            self.alpha = alpha;
         pygame.sprite.Sprite.__init__(self);
-        self.x = x - self.r;
-        self.y = y - self.r;
-        self.alpha = 0;
+        self.x = x;
+        self.y = y;
+        self.frame_counter = 0;
+        self.image_original =[pygame.transform.scale(pygame.image.load("../gfx/cop_0.png"), (100, 100)), pygame.transform.scale(pygame.image.load("../gfx/cop_1.png"), (100, 100))];
         
     def update(self):
-        self.image = pygame.surface.Surface((self.r*2, self.r*2));
-        self.rect = self.image.get_rect();
-        pygame.draw.circle(self.image, pygame.Color(0, 0, 128), self.rect.center, self.r);
-        pygame.draw.line(self.image, pygame.Color(0, 0, 0), self.rect.center, (self.rect.center[0] + math.cos(self.alpha) * self.r, self.rect.center[1] + math.sin(self.alpha) * self.r));
-        self.image.set_colorkey(pygame.Color('black'));
-        self.rect.center = (self.x, self.y);
+        self.frame_counter +=1;
+        self.image = pygame.transform.rotate(self.image_original[(self.frame_counter / 100) % len(self.image_original)], self.alpha);
+        self.rect = self.image.get_rect(center=(self.x, self.y));
         
     def SetPosition(self, x, y, alpha):
         self.x = x - self.r;
         self.y = y - self.r;
-        self.alpha += alpha;
-        
-        
+        self.alpha = -alpha;
 
 class Cop():
     def InitMovement(self, start, end):
-        self.direction = (self.end[0] - self.start[0], self.end[1] - self.start[1]);
+        self.direction = (end[0] - start[0], end[1] - start[1]);
         direction_length = pow(pow(self.direction[0], 2) + pow(self.direction[1], 2), 0.5);
         if (direction_length == 0):
             self.direction = (0, 0);
@@ -35,22 +34,49 @@ class Cop():
             self.direction = (self.direction[0]/direction_length, self.direction[1]/direction_length); 
         self.x = start[0];
         self.y = start[1];
-        self.rotation_time = 0;
-        self.sign = 1;
+        self.speed = 30;
+        self.rotation_speed = 30;
+        self.path_length = direction_length;
+        self.destination_angle = math.degrees(math.acos(self.direction[0]));
+
+        if (math.copysign(1, self.direction[1]) != math.copysign(1, math.sin(self.destination_angle))):
+            self.destination_angle = 360 - self.destination_angle;
+                
+        self.start = start;
+        self.end = end;
         
     def __init__(self, starting_x, starting_y, destination_x, destination_y):
-        self.cop_sprite = CopSprite(starting_x, starting_y);
         self.start = (starting_x, starting_y);
         self.end = (destination_x, destination_y);
+        self.destination_angle = 0;
+        self.angle = 0;
         self.InitMovement(self.start, self.end);
-        self.speed = 10;
+        self.cop_sprite = CopSprite(starting_x, starting_y, self.angle);
+        self.angle = self.destination_angle;
     
     def GetSprite(self):
         return self.cop_sprite;
     
     def update(self, dt):
-        self.rotation_time += dt;
-        if (self.rotation_time > 1000):
-            self.sign = random.randint(0, 2) - 1;
-            self.rotation_time = 0;
-        self.cop_sprite.SetPosition(self.x, self.y, self.sign * 0.00405);
+        if (self.destination_angle != self.angle):
+            if (math.fabs(self.destination_angle - self.angle) < dt * self.rotation_speed):
+                self.angle = self.destination_angle;
+            else:
+                dangle = math.copysign(dt * self.rotation_speed, self.destination_angle - self.angle);
+                self.angle += dangle;
+            if (self.angle < 0):
+                self.angle += 360;
+            if (self.angle > 360):
+                self.angle -=360;
+        else:
+            dx = self.direction[0] * dt * self.speed;
+            dy = self.direction[1] * dt * self.speed;
+            self.x = self.x + dx;
+            self.y = self.y + dy;
+
+            if (pow(pow(self.x - self.start[0], 2) + pow(self.y - self.start[1], 2), 0.5) >= self.path_length):
+                new_start = self.end;
+                new_end = self.start;
+                self.InitMovement(new_start, new_end);
+        
+        self.cop_sprite.SetPosition(int(self.x), int(self.y), self.angle);
