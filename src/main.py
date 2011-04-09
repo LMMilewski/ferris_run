32a7dad154e25cc8a357e53a60770a6685b1941b
Lukasz Milewski <lmmilewski@gmail.com>
2011-04-09 23:08:46 +0200

    make sister and director faster on later levels

--- src/main.py
+++ src/main.py
@@ -610,9 +610,15 @@ class FerrisRunGame(GameState):
             self.ferris.update(dt / self.cfg.bullet_slowdown_factor, self.cars)
         else:
             self.ferris.update(dt, self.cars)
-        self.director.update(dt if self.level_num > 1 else dt * 0.5, self.cars)
+
+        director_dt = dt if self.level_num > 1 else dt * 0.5
+        director_dt = dt if self.level_num < 6 else dt * 1.2
+        self.director.update(director_dt, self.cars)
+
         if self.level_num >= 2:
-            self.sister.update(dt if self.level_num >= 3 else dt * 0.5, self.cars)
+            sister_dt = dt if self.level_num >= 3 else dt * 0.5
+            sister_dt = dt if self.level_num <  7 else dt * 1.2
+            self.sister.update(sister_dt, self.cars)
         self.register.update(dt)
         for bonus in self.bonuses:
             if self.bullet_time:
